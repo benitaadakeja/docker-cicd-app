1199f5592a08e2b551aeb4e03b5b37f80a1c674f
Benita Adakeja <benitaadakeja@gmail.com>
2026-08-24 13:52:41 +0100

    Update test for deployment message

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,7 @@ def test_home_route():
 	client = app.test_client()
 	response = client.get("/")
 	assert response.status_code == 200
-	assert response.data.decode() == "Hello from CI/CD and Docker applications!"
+	assert response.data.decode() == "Hello from automated CI/CD deployment from AWS EC2!"
 
 def test_health_check():
 	client = app.test_client()
